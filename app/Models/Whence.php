a406d5b46efa68a51c92681a8b6a8ad1395f8f27
Rafał Majkowski <majkipl@gmail.com>
2018-10-04 16:03:03 +0200

    add whence model, factory, seeder and migration

---
+++ app/Models/Whence.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Cache;
+
+class Whence extends Model
+{
+    protected $fillable = ['id', 'name'];
+
+    public $timestamps = false;
+
+    /**
+     * @return HasMany
+     */
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getAllCached()
+    {
+        $cacheKey = (new self)->getTable();
+
+        return Cache::remember($cacheKey, now()->addDay(365), function () {
+            return self::all();
+        });
+    }
+}
