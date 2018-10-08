6f616493c55d05af65abde707232a1dcf54de00d
Rafał Majkowski <majkipl@gmail.com>
2018-10-08 09:03:40 +0200

    modify user and error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.front')
+
+@section('content')
+    <section class="e404" id="e404">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-12 my-4">
+                    <h1>404</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
